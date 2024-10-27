16658ef4bd2231b8a38be9393c38f24db8082467
nlemu <nlemuvagyok@gmail.com>
2024-10-27 22:53:42 +0100

    2

--- homework/find_a_string/find_a_string.py
+++ homework/find_a_string/find_a_string.py
@@ -1,3 +1,30 @@
 def find_a_string(original_str: str, substr: str) -> int:
-    """Write your code here"""
-    pass
+    number = 0
+    if substr == "":
+        number = number
+    else:
+        for i in range(0, len(original_str) - len(substr) + 1):
+            if substr == original_str[i: i + len(substr)]:
+                number = number + 1
+    return number
+
+
+def find_sum (*numbers):
+    result = 0
+
+    for num in numbers:
+        result = result + num
+
+    print("The result of the numbers is:", result)
+
+find_sum(3, 4, 10)
+
+
+
+# In this task, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string.
+# String traversal will take place from left to right, not from right to left.
+
+
+
+
+
