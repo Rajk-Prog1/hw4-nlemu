2c46d2097571c05cd29c7e6a54fa76783ac3dc30
nlemu <nlemuvagyok@gmail.com>
2024-10-28 00:04:19 +0100

    ezt előbb pusholtam?

--- homework/find_a_string/find_a_string.py
+++ homework/find_a_string/find_a_string.py
@@ -9,18 +9,6 @@ def find_a_string(original_str: str, substr: str) -> int:
     return number
 
 
-def find_sum (*numbers):
-    result = 0
-
-    for num in numbers:
-        result = result + num
-
-    print("The result of the numbers is:", result)
-
-find_sum(3, 4, 10)
-
-
-
 # In this task, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string.
 # String traversal will take place from left to right, not from right to left.
 
